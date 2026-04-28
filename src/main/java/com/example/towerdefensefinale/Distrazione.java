effac5657e3630ae5b5ef541f33812093eff9f35
Abazovski Alen <alen.abazovski@cz.lan>
2026-04-28 14:39:46 +0200

    distrazione

--- src/main/java/com/example/towerdefensefinale/Distrazione.java
+++ src/main/java/com/example/towerdefensefinale/Distrazione.java
@@ -1,5 +1,21 @@
 package com.example.towerdefensefinale;
+import  javafx.scene.paint.Color;
 
 public class Distrazione {
     private double x, y;
+    private String tipo;
+    private Color colore;
+
+    public Distrazione(double x , double y, String tipo){
+        this.x = x;
+        this. y = y;
+        this.tipo = tipo;
+
+
+        if(tipo.equals(("Ciotola"))){
+            this.colore = Color.ORANGE;
+        }else{
+            this.colore = Color.PINK;
+        }
+    }
 }
